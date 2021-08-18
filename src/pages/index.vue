1fbadc7df8b3acae9ec9965c7340eeb98faf8cc4
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 11:46:31 +0200

    feat(index): add button to draw random number

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <h3>Hello World</h3>
+  <div class="mb-13">
+    <header>
+      <h1 class="text-5xl">Welcome</h1>
+    </header>
+    <section class="mt-10">
+      <p>Draw a random number</p>
+      <button
+        class="m-3 text-sm btn outline-none font-bold tracking-widest"
+        @click="drawRandomNum"
+      >
+        DRAW
+      </button>
+      <p>{{ randomNum }}</p>
+    </section>
   </div>
 </template>
+
+<script setup lang="ts">
+let randomNum = ref(0)
+
+const drawRandomNum = () => (randomNum.value = Math.round(Math.random() * 100))
+</script>
+
+
+<style scoped>
+.outline-none {
+  outline: none;
+}
+</style>
